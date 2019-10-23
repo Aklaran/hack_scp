5feafa8da1a8e8923f7c6352728c94df7602a312
Matt Kern <mjkern@andrew.cmu.edu>
2019-10-23 17:35:27 -0400

    scraper now gets id, href, and title of korean articles translated to english

--- webScraping/KoreanSpider.py
+++ webScraping/KoreanSpider.py
@@ -10,10 +10,9 @@ class KoreanSpider(Spider):
         items = []
         for row in rows:
             item = {}
-            #item['line'] = row.xpath(".").extract()
             item['scpId'] = row.xpath("./a/text()").extract()
-#             item['category'] = row.xpath("./h4/a/text()").extract()
-#             item['appstore_link_url'] = row.xpath("a[1]/@href")[0].extract()
-#             item['img_src_url'] = row.xpath("./a/img/@src").extract()
+            item['href'] = row.xpath("./a/@href").extract()
+            item['name'] = row.xpath("./text()").extract()[0][3:]
+            # ^^^ indexing and list slice drops the preceding " - " ^^^
             items.append(item)
         return items
