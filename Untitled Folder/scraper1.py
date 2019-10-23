fbadb8d8c9ea617fe8eb1fae6c43e558b5d62845
Matt Kern <mjkern@andrew.cmu.edu>
2019-10-23 16:59:03 -0400

    simple example

--- Untitled Folder/scraper1.py
+++ Untitled Folder/scraper1.py
@@ -2,14 +2,15 @@ from scrapy.spiders import Spider
 
 class MySpider(Spider):
     name = 's1'
-    allowed_domains = ['apple.com']
-    start_urls = [ "https://www.apple.com/itunes/charts/free-apps/" ]
+    allowed_domains = ['scp-int.wikidot.com']
+    start_urls = [ "http://scp-int.wikidot.com/members-pages" ]
 
     def parse(self, response):
-        rows = response.xpath("/html[@class='gr__scp-int_wikidot_com']/body[@id='html-body']/div[@id='skrollr-body']/div[@id='container-wrap-wrap']/div[@id='container-wrap']/div[@id='container']/div[@id='content-wrap']/div[@id='main-content']/div[@id='page-content']/table[@class='wiki-content-table']/tbody/tr")
+        rows = response.xpath("//table/tr")
         items = []
         for row in rows:
             item = {}
+            #item['line'] = row.xpath(".").extract()
             item['author'] = row.xpath("./td[1]/span[@class='printuser avatarhover']/a/text()").extract()
 #             item['category'] = row.xpath("./h4/a/text()").extract()
 #             item['appstore_link_url'] = row.xpath("a[1]/@href")[0].extract()
