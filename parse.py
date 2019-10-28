1bbc934ff605ea05eb719b8fc5044343aa3f3bbd
mkornyev <mkornyev@gmail.com>
2019-10-28 13:13:32 -0400

    Got parse.py to give clean values for entropy, created data directories, added vizualization ipynb doc

--- parse.py
+++ parse.py
@@ -12,7 +12,7 @@ def getKLD(filename1, filename2):
 		file2 = open(filename2, 'r', encoding="utf8")
 	except:
 		return "None"
-		
+
 	text1 = file1.read().split(" ")
 	text2 = file2.read().split(" ") 
 
@@ -47,8 +47,32 @@ def getKLD(filename1, filename2):
 	relativeKLD = entropy(text1_data, text2_data) 
 	return relativeKLD
 
+def getEntropy(file, splitOnChars = False):
+	try:
+		file1 = open(file, 'r',encoding="utf8")
+	except:
+		return "None"
 
-def parse(file):
+
+	if splitOnChars: 
+		text1 = list(file1.read()) # Need to split on characters for languages like chinese
+	else: 
+		text1 = file1.read().split(" ")
+
+	# Get probability distributions
+	dist1 = {}
+	for word in text1:
+		dist1[word] = dist1.get(word, 0) + 1
+
+	# Normalize
+	for word in dist1:
+		dist1[word] = (dist1[word] / len(dist1))
+
+	text1_data = list(dist1.values())
+
+	return entropy(text1_data) 
+
+def parse(file, parseChars = False):
 	df = pd.read_csv(file)
 	languageMap = {"cn":"chinese",
 					"jp":"japanese",
@@ -61,7 +85,9 @@ def parse(file):
 					"es":"spanish",
 					"th":"thai"}
 
-	newCol = []
+	originalCol = []
+	translateCol = []
+	differenceCol = []
 	for language in languageMap:
 		if language in df["href"][1]:
 			lang = languageMap[language]
@@ -69,12 +95,35 @@ def parse(file):
 	for i in df.index:
 		opath = "data/" + lang + "Originals" + df["href"][i] + ".txt"
 		tpath = "data/" + "englishFrom" + lang + df["href"][i] + ".txt"
-		result = getKLD(opath, tpath)
-		newCol.append(result)
-	assert(len(newCol) ==  df.shape[0])
-	df["kld"] = newCol
+
+		# Cleaning any E notation syntax using the double() func 
+		try:
+			origString = double(getEntropy(opath, parseChars))
+		except Exception as e: 
+			origString = getEntropy(opath, parseChars)
+
+		try:
+			transString = double(getEntropy(tpath, parseChars))
+		except Exception as e:
+			transString = getEntropy(tpath, parseChars)
+
+		originalCol.append(origString)
+		translateCol.append(transString)
+		
+		# try:
+		# 	differenceCol.append(float(getEntropy(opath))- float(getEntropy(tpath)))	
+		# except Exception as e:
+		# 	pass # Format error 
+		
+
+	assert(len(originalCol) ==  len(translateCol) == df.shape[0])
+	df["Original Entropy"] = originalCol
+	df["Translated Entropy"] = translateCol
+	# df["Change in E"] = differenceCol
+	# header = columns = ['scpId','href','englishRating','englishDate','englishAuthor','chineseRating','chineseDate','chineseAuthor','chineseAuthorKudos','Original E','Translated E']
+	# df.insert(11, "Change in Entropy", differenceCol, allow_duplicates=True)
 	
-	outfile = "data/kld/" + lang + "-kld.csv"
+	outfile = "data/kld/" + lang + "-entropy.csv"
 	df.to_csv(outfile, index = False)
 
 if __name__=="__main__":
@@ -84,4 +133,7 @@ if __name__=="__main__":
 	"data/thai.csv"]
 
 	for file in files:
-		parse(file)
+		if file == "data/chinese.csv" or file == "data/korean.csv" or  file == "data/thai.csv" or file == "data/japanese.csv":
+			parse(file, parseChars = True)
+		else: 
+			parse(file)
